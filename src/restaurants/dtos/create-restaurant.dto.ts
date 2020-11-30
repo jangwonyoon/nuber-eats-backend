0bf16b72c1e2ff0c301baeffb0a3916d36347b01
TheJang <wkddnjs7310@naver.com>
2020-11-30 16:45:13 +0900

    Dto validator, class-validator, class-transformer

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,16 +1,24 @@
 import { Field, InputType, Args, ArgsType } from '@nestjs/graphql';
-
+import { IsBoolean, IsString, Length } from 'class-validator';
 /* Data transfer  object */
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field((type) => Boolean)
   isVegan: boolean;
+
   @Field((type) => String)
+  @IsString()
   address: string;
+
   @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
   ownersName: string;
 }
 
@@ -18,4 +26,14 @@ export class CreateRestaurantDto {
 
  @ArgsType은 기본적으로 분리된 값들을 GraphQL argument로 전달해 줄 수 있도록 해준다.
 
+*/
+
+/* npm i class-validator로 모듈을 가져오고 
+    dto 유효성 검사를 실시 한다. 
+
+    main.ts에 아래 구문을 작성
+
+    app.useGlobalPipes(new ValidationPipe());
+
+    npm i class-transformer 넣기 
 */
