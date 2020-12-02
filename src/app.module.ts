7d23986b245d4dcc985a26607cc6c8756cf65fd1
TheJang <wkddnjs7310@naver.com>
2020-12-02 17:26:58 +0900

    .env setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 /* GraphQLModule을 가져와서 forRoot()함수로 루트 모듈을 설정 
 
@@ -22,8 +23,18 @@ autoSchemaFile: true,을 통해 gql을 생성 하지 않아도 된다.
 
 /* Postgres는 localhost연결에서는 password를 추적하지 않는다. */
 
+/* ConfigModule에서 isGlobal은 우리의 애플리케이션의 어디서나 config 모듈에 접근할 수 있다는 거다.
+  
+  npm i cross-env 설치를 통해서 나의 환경이 macOS이던지 , Window이던지 Linux이던지 상관없이 쓸 수 있게 해준다. 
+
+*/
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
